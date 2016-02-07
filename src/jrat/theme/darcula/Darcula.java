f17b35886ca0f029c19864a37136df44a95b60e5
redpois0n <red@cmail.nu>
2016-02-07 18:48:15 +0100

    Use icon in plugin

--- src/jrat/theme/darcula/Darcula.java
+++ src/jrat/theme/darcula/Darcula.java
@@ -1,13 +1,14 @@
 package jrat.theme.darcula;
 
 import com.bulenkov.darcula.DarculaLaf;
+import iconlib.IconUtils;
 import javax.swing.UIManager;
 import jrat.api.Plugin;
 
 public class Darcula extends Plugin {
 
 	public Darcula() {
-		super("Darcula Theme", "1.0", "Dark Theme", "jRAT");
+		super("Darcula Theme", "1.0", "Dark Theme", "jRAT", IconUtils.getIcon("icon", Darcula.class));
 
 		System.setProperty("jrat.theme", "false");
 
